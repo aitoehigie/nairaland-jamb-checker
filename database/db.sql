a92b984f7ee4352b23edf88eb4cd239c2b8a6147
Ehigie (Pystar) Aito <aitoehigie@gmail.com>
2012-04-07 08:03:18 +0100

    Added database definition and cleaned up parts of the code,again

--- database/db.sql
+++ database/db.sql
@@ -1,3 +1,6 @@
+
+
+
 CREATE TABLE `students` (
 `id` INTEGER NOT NULL AUTO_INCREMENT ,
 `student name` VARCHAR(100) NOT NULL ,
@@ -8,4 +11,13 @@ CREATE TABLE `students` (
 PRIMARY KEY (`id`)
 ) COMMENT 'this table contains the vital information of candidates.';
 
+CREATE TABLE `subjects` (
+`id` INTEGER NOT NULL AUTO_INCREMENT ,
+`student_id` INTEGER NOT NULL ,
+`subject 1` VARCHAR(32) NOT NULL ,
+`subject 2` VARCHAR(32) NOT NULL ,
+`subject 3` VARCHAR(32) NOT NULL ,
+`subject 4` VARCHAR(32) NOT NULL ,
+PRIMARY KEY (`id`)
+) COMMENT 'This contains the 4 subjects taken by the jamb candidate.';
 
